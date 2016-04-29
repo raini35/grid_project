da642c1780abd2e626c18ce9a469652f733c7ac6
raini35 <rainierfgo@Rainiers-MacBook-Pro.local>
2016-04-29 19:09:26 -0400

    -A

--- js/grid.js
+++ js/grid.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
     var $newdiv;
-    for (var i = 0; i < 100; i++) {
-    	var limit = 
-        $newdiv = $('<div class="ball" />').text(i);
-        $('body').append($newdiv);
+    for (var i = 1; i <= 100; i++) {
+ 		if ((i % 10)==0 ) {
+       		$newdiv = $('<div class="balls"/><br>').text(i);
+        	$('body').append($newdiv);
+    	} 
+    	else {
+     		$newdiv = $('<div class="ball"/>').text(i);
+        	$('body').append($newdiv);
+    	}
     }
 });
+

207f11a44965dedae47fa17d8146e6cc0567fb26
raini35 <rainier.f.go@gmail.com>
2016-04-30 19:56:47 -0400

    Added files via upload

--- js/grid.js
+++ js/grid.js
@@ -1,44 +1,63 @@
 var height = prompt("Enter the height: ");
 var width = prompt("Enter the width: "); 
-
-/*$(document).ready(function() {
-var &newdiv; 
-for (var h = 1; h<= height; h++) {
-	for( var w = 1; w <= width; w++) {
-		if(w % root == 0)( {
-			$newdiv = $('<div class="balls"/><br>').text(i);
-        	$('body').append($newdiv);
-		}
-		else 
-		{
-			$newdiv = $('<div class="ball"/>').text(i);
-        	$('body').append($newdiv);
-		}
-	}
-}); 
-*/
+var newHeight = 34* height;  
+var newWidth = 34 * width; 
+var number = 1; 
 
 $(document).ready(function () {
     var $newdiv;
-    for (var w = 1; w <=height; w++) {
-    for (var i = 1; i <= width; i++) {
- 		if ((i % width)==0 ) {
-       		$newdiv = $('<div class="balls"/><br>');
-        	$('body').append($newdiv);
-    	} 
-    	else {
-			$newdiv = $('<div class="ball"/>');
-        	$('body').append($newdiv);
-    	}
-    }
-    }
+    
+    makeBoard(); 
+    
+    $('.ball').on("mouseover", function() {
+    	var $c= findColor(number); 
+    	
+    	 $(this).css("background", $c);
+  
+		number = newNumber(number); 
+    }); 
+    
+    $('#button').on("click", function() {
+    	$('.ball').css("background", "white"); 
+    }); 
+    
+    $('#button2').on("click", function() {
+    	makeBoard(); 
+    }); 
+    
+    
 });
 
-/* 
-There will be two inputs 
+function makeBoard(){
+$('#container').width(newWidth).height(newHeight); 
+    
+    for (var w = 1; w <=height; w++) {
+    	for (var i = 1; i <= width; i++) {
+ 			if ((i % width)==0 ) {
+       			$newdiv = $('<div class="ball"/><br>');
+        		$('#container').append($newdiv);
+    		}	 
+    		else {
+				$newdiv = $('<div class="ball"=/>');
+        		$('#container').append($newdiv);
+    		}
+    	}
+    }
+}
+function findColor(x) {
+	if(x == 1) 
+		return "black"; 
+	//else if (x == 2) 
+	//	return "white"; 
+	//else (x == 3)
+	//	return "blue"; 
+}
 
-for loop for the height 
-and a while loop for the width 
-
-
-*/
+function newNumber(x) {
+	if(x == 1) 
+		return 1; 
+	//else if (x == 2) 
+	//	return 1; 
+	//else (x == 3)
+	//	return 1; 
+}
